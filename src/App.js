1d528f842db1acea73747ecfbbd823de01dfd2fd
seamus2002 <seamus.english58@myhunter.cuny.edu>
2022-04-17 13:46:08 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Axios from 'axios';
 
 function App() {
+  const [pokemon, setPokemon] = useState('');
+  const [isSelected, setIsSelected] = useState(false);
+  const [info, setInfo] = useState({
+    name: '',
+    species: '',
+    type1: '',
+    type2: '',
+    img: '',
+  });
+
+  const searchPokemon = () => {
+    Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    .then((response) => {
+      setInfo({
+        name: pokemon,
+        species: response.data.species.name,
+        type: response.data.types.map(type => type.type.name).join(", "),
+        img: response.data.sprites.front_default,
+      });
+      setIsSelected(true);
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='top-banner'>
+        <h1 className='banner-text'>Welcome to the Pokedex</h1>
+        <br />
+        <input type='text' onChange={(event) => setPokemon(event.target.value)} />
+        <br />
+        <br />
+        <button type="button" className="btn btn-light" onClick={searchPokemon}>Search</button>
+      </div>
+      
+      <div className='display'>
+        {!isSelected ? (
+          <h1 className='pokemon-name'>Search for a Pokemon!</h1>
+        ) : (
+          <>
+            <h2 className='text-capitalize pokemon-name'>{info.species}</h2>
+            <h2 className='text-capitalize'>Type: {info.type}</h2>
+            <img src={info.img} />
+          </>)}
+      </div>
     </div>
   );
 }
